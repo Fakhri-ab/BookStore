d9b8b7d702d6340b06394b924c49deb176869328
Fakhri-ab <92473176+Fakhri-ab@users.noreply.github.com>
2024-10-07 13:33:40 +0100

    first commit

---
+++ book-network-back/src/main/java/com/fakhri/book/feedback/FeedbackService.java
@@ -0,0 +1,62 @@
+package com.fakhri.book.feedback;
+
+import com.fakhri.book.book.Book;
+import com.fakhri.book.book.BookRepository;
+import com.fakhri.book.common.PageResponse;
+import com.fakhri.book.exception.OperationNotPermittedException;
+import com.fakhri.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+
+    private final FeedBackRepository feedBackRepository;
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + request.bookId()));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for and archived or not shareable book");
+        }
+
+         User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getCreatedBy(), connectedUser.getName())) {
+            throw new OperationNotPermittedException("You cannot give feedback to your own book");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedBackRepository.save(feedback).getId();
+
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+         Pageable pageable = PageRequest.of(page, size);
+        User user = ((User) connectedUser.getPrincipal());
+        Page<Feedback> feedbacks = feedBackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
+    }
+}
---
+++ book-network-back/src/main/java/com/fakhri/book/history/BookTransactionHistory.java
@@ -0,0 +1,34 @@
+package com.fakhri.book.history;
+
+import com.fakhri.book.book.Book;
+import com.fakhri.book.common.BaseEntity;
+import com.fakhri.book.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private boolean returned;
+    private boolean returnApproved;
+}
